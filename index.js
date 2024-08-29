03bfb6ce4d0a43cc188a403b667aba0b672b2c7b
Luis Henrique <129170907+luish2807@users.noreply.github.com>
2024-08-29 01:27:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const primeiroNome = "Luis Henrique";
+const sobrenome = "Meurer";
+const idade = 16;
+const altura = 1.75;
+const massa = 80;
+const cidadeNatal = "Curitiba";
+
+  console.log("O meu primeiro nome é:")
+  console.log(primeiroNome);
+  console.log("O sobrenome da minha fámilia é:")
+  console.log(sobrenome);
+  console.log("Minha idade é:")
+  console.log(idade);
+  console.log("Minha altura é:")
+  console.log(altura);  
+  console.log("Meu peso é:")
+  console.log(massa);
+  console.log("Eu sou nascido na cidade de:")
+  console.log(cidadeNatal);
+  
